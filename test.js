5564c79f73dcde61c40b913d66e5b2d95eb9430d
charles <charles.onuorah@yahoo.com>
2018-05-18 16:46:35 +0100

    hub.js

--- test.js
+++ test.js
@@ -16,15 +16,13 @@ import path from 'path';
 import fs from 'fs';
 import express from 'express';
 import routes from './route/index';
-import hub from './hub';
-//set enviroment variables
-import {getusers} from './hub';
+import {getusers,getUserRequest} from './hub';
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get(homeUrl + 'api/v1/users', (req,res) => hub.getusers(req,res));
-
+app.get(`${homeUrl}api/v1/users`, (req, res) => getusers(req, res));
+app.get(`${homeUrl}api/v1/users:id/requests`, (req, res) => getUserRequest(req, res));
 
 
 export default app;
